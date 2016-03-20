2a21e2141415f76802dd1d80d7c98581f6a3baf1
Kevin Dethlefs <kdethlefs@gmail.com>
2016-03-20 11:44:15 -0500

    Finishing the skeleton for code.
    *Assuming this is going to run on crontab

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import sys
 
 config_file = "config/app.settings" # Configuration file, this is the default setting.
 log_file = "logs/client.log" # Log file, default setting.
-clients = {} #Dictionary listing clients IP addresses.
-
+servers = {"localhost"} #Dictionary listing clients IP addresses.
+debug = True #set to false to run on production system.
 
 # Open and init the log file.
 logFile = open(log_file, "w", -1)
@@ -13,9 +13,24 @@ def log(message):
     timestamp = datetime.datetime.now()
     completeMessage = str(timestamp) + ": " + message
     logFile.write(completeMessage)
-    print(completeMessage)
+    if debug:
+        print(completeMessage)
     
 log("Starting Robobrew")
 
+# Start defining functions here.
+def getTemp():
+    #Pull the temp from the I2C probe. Return it.
+    return 0
+
+def sendToServers(temp):
+    #For each server in servers, send to the REST api
+    for server in servers:
+        log("Sending to server " + server)
+        #TODO: Finish this
 # Read configuration file and set variables appropriately. If missing value, exit.
-sys.exit(1)
+
+# Gather temp and send it
+sendToServers(getTemp())
+
+logFile.close()
