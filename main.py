368a60d9c88a77ee40dd5686a2eb0c2fccab3525
Kevin Dethlefs <kevindethlefs@unomaha.edu>
2016-10-01 23:22:10 +0000

    Taling to i2c. start, WIP

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python
 import datetime
 import sys
+import smbus
+import time
+import ADS1x15
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(11, GPIO.OUT)
+
+adc = ADS1x15.ADS1115()
+
+DEVICE_ADDRESS = 0x48
+GAIN = 1
 
 config_file = "config/app.settings" # Configuration file, this is the default setting.
 log_file = "logs/client.log" # Log file, default setting.
@@ -21,6 +33,29 @@ log("Starting Robobrew")
 # Start defining functions here.
 def getTemp():
     #Pull the temp from the I2C probe. Return it.
+    #I2C address: 0x48 (1 to read, 0 to write)
+	
+    log("Starting conversion.")
+    channel0 = adc.read_adc(0, 1)
+    channel1 = adc.read_adc(1, 4)
+    channel3 = adc.read_adc_difference(1, gain=4)
+    
+    log("Channel 0: " + str(channel0))
+    log("Channel 1: " + str(channel1))
+    log("Channel 3: " + str(channel3))
+    
+    GPIO.output(11, GPIO.HIGH)
+    time.sleep(.02)
+
+
+    channel0 = adc.read_adc(0, gain=GAIN)
+    channel1 = adc.read_adc(1, gain=4)
+    channel3 = adc.read_adc(3, gain=4)
+    GPIO.output(11, GPIO.LOW)
+    log("Channel 0: " + str(channel0))
+    log("Channel 1: " + str(channel1))
+    log("Channel 3: " + str(channel3))
+    
     return 0
 
 def sendToServers(temp):
